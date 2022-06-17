a4fa7ca6bf7c6eab40d07d3d4a517e58847a3c50
leesungeon <devcez82@gmail.com>
2022-06-17 20:57:15 +0900

    Day01
    Printing
    Commenting
    Debugging
    String Manipulation and Variables

---
+++ day01/03_input-function.py
@@ -0,0 +1,6 @@
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+# input("What is your name?")
+print("Hello " + input("What is your name?") + "!")
+
+print(len(input("What is your name?")))
