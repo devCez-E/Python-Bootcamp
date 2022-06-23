810c96dbdba6e7a3a4f4ba6a76894c10abccb402
sungeonlee <devcez82@gmail.com>
2022-06-23 17:56:01 +0900

    Day6

---
+++ day06/01_function-basic.py
@@ -0,0 +1,14 @@
+#print, len 등도 하나의 함수
+
+#def my_function():
+#   Do this
+#   Then do this
+#   Finally do this
+
+#my_function() ------> Calling Functions "AFTER" defined.
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
---
+++ day06/02_while-loop.py
@@ -0,0 +1,9 @@
+number_of_nurdles = 6
+
+def greeting(number):
+    print(f"Hello {number}")
+
+while number_of_nurdles > 0:
+    greeting(number_of_nurdles)
+    number_of_nurdles -= 1
+    
