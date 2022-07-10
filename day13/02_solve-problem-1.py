163c826d02de0708b01aae298b2044882bb6cb3d
leesungeon <devcez82@gmail.com>
2022-07-10 17:37:49 +0900

    Day13

---
+++ day13/02_solve-problem-1.py
@@ -0,0 +1,13 @@
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# # Describe Problem
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+my_function()
---
+++ day13/09_exercise-2.py
@@ -0,0 +1,24 @@
+#year = input("Whitch year do you want to check?")
+#if year % 4 == 0:
+#    if year % 100 == 0:
+#        if year % 400 == 0:
+#            print("Leap year.")
+#        else:
+#            print("Not leap year.")
+#    else:
+#        print("Leap year.") 
+#else:
+#    print("Not leap year.")       
+
+
+year = int(input("Whitch year do you want to check?"))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.") 
+else:
+    print("Not leap year.")       
